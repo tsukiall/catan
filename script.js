97a131f04f57aba5483f477980fbd430bc18f88b
Alex Asenov <tsukiall@gmail.com>
2026-07-16 18:12:27 +0300

    Version update

--- script.js
+++ script.js
@@ -5,117 +5,19 @@ const boardConfig = {
     rows: [3, 4, 5, 4, 3],
     offset: [1, 0.5, 0, 0.5, 1],
     pool: createPool({ lumber: 4, wool: 4, grain: 4, brick: 3, ore: 3, desert: 1 }),
-    tokens: [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12],
-    harbours: [
-      {
-        location: '0-1',
-        type: '3:1',
-        rotation: '-30deg'
-      },
-      {
-        location: '0-2',
-        type: '3:1',
-        rotation: '30deg'
-      },
-      {
-        location: '1-3',
-        type: 'Brick',
-        rotation: '90deg'
-      },
-      {
-        location: '3-3',
-        type: 'Lumber',
-        rotation: '90deg'
-      },
-      {
-        location: '4-2',
-        type: '3:1',
-        rotation: '150deg'
-      },
-      {
-        location: '4-1',
-        type: 'Grain',
-        rotation: '-150deg'
-      },
-      {
-        location: '3-0',
-        type: 'Ore',
-        rotation: '-150deg'
-      },
-      {
-        location: '2-0',
-        type: '3:1',
-        rotation: '-90deg'
-      },
-      {
-        location: '1-0',
-        type: 'Wool',
-        rotation: '-30deg'
-      },
-    ]
+    tokens: [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11],
+    harbourTypes: ['any', 'any', 'brick', 'lumber', 'any', 'grain', 'ore', 'any', 'wool'],
+    harbourLocations: ['-1-1', '-1-3', '1-4', '3-4', '5-3', '5-1', '4--1', '2--1', '0--1'],
+    harbourRotations: ['60deg', '120deg', '180deg', '180deg', '240deg', '300deg', '300deg', '0', '60deg']
   },
   6: {
     rows: [4, 5, 6, 6, 5, 4],
     offset: [1, 0.5, 0, 0.5, 1, 1.5],
     pool: createPool({ lumber: 6, wool: 6, grain: 6, brick: 5, ore: 5, desert: 2 }),
-    tokens: [2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 8, 8, 8, 9, 9, 9, 10, 10, 10, 11, 11, 11, 12, 12],
-    harbours: [
-      {
-        location: '0-1',
-        type: '3:1',
-        rotation: '-30deg'
-      },
-      {
-        location: '0-3',
-        type: '3:1',
-        rotation: '30deg'
-      },
-      {
-        location: '1-4',
-        type: 'Brick',
-        rotation: '90deg'
-      },
-      {
-        location: '3-5',
-        type: '3:1',
-        rotation: '30deg'
-      },
-      {
-        location: '4-4',
-        type: 'Wood',
-        rotation: '90deg'
-      },
-      {
-        location: '5-3',
-        type: '3:1',
-        rotation: '150deg'
-      },
-      {
-        location: '5-2',
-        type: 'Grain',
-        rotation: '-150deg'
-      },
-      {
-        location: '5-0',
-        type: 'Wool',
-        rotation: '150deg'
-      },
-      {
-        location: '4-0',
-        type: 'Ore',
-        rotation: '-150deg'
-      },
-      {
-        location: '2-0',
-        type: '3:1',
-        rotation: '-90deg'
-      },
-      {
-        location: '1-0',
-        type: 'Wool',
-        rotation: '-30deg'
-      },
-    ]
+    tokens: [2, 5, 4, 6, 3, 9, 8, 11, 11, 10, 6, 3, 8, 4, 8, 10, 11, 12, 10, 5, 4, 9, 5, 9, 12, 3, 2, 6],
+    harbourTypes: ['any', 'any', 'brick', 'wool', 'lumber', 'any', 'grain', 'any', 'ore', 'any', 'wool'],
+    harbourLocations: ['-1-1', '-1-4', '1-5', '2-6', '4-5', '6-4', '6-2', '6-1', '5--1', '2--1', '0--1'],
+    harbourRotations: ['60deg', '120deg', '180deg', '120deg', '180deg', '240deg', '300deg', '240deg', '300deg', '0', '60deg']
   }
 }
 
@@ -131,6 +33,155 @@ const shuffle = (pool) => {
   return pool;
 }
 
+const getNeighbour = (config, row, col, direction) => {
+  let nextRow = row, nextCol = col;
+
+  switch(direction) {
+    case 0:
+      nextCol = col + 1;
+      break;
+    case 1:
+      nextRow = row + 1;
+      nextCol = Math.round(config.offset[row] + col + 0.5 - config.offset[row + 1]);
+      break;
+    case 2:
+      nextRow = row + 1;
+      nextCol = Math.round(config.offset[row] + col - 0.5 - config.offset[row + 1]);
+      break;
+    case 3:
+      nextCol = col - 1;
+      break;
+    case 4:
+      nextRow = row - 1;
+      nextCol = Math.round(config.offset[row] + col - 0.5 - config.offset[row - 1]);
+      break;
+    case 5:
+      nextRow = row - 1;
+      nextCol = Math.round(config.offset[row] + col + 0.5 - config.offset[row - 1]);
+      break;
+  }
+
+  return { r: nextRow, c: nextCol, key: `${nextRow}-${nextCol}` };
+}
+
+const generateTile = (board, type, row, col, offset) => {
+  const hex = document.createElement('div');
+  hex.classList.add('hex', type);
+  hex.style.setProperty('--row', row);
+  hex.style.setProperty('--col', col);
+  hex.style.setProperty('--row-offset', offset);
+  hex.dataset.coords = `${row}-${col}`;
+
+  board.appendChild(hex);
+}
+
+const generateTokens = (config) => {
+  const unvisited = new Set();
+  const coastTiles = [];
+  const maxRows = config.rows.length;
+
+  config.rows.forEach((row, rowIndex) => {
+    for (let colIndex = 0; colIndex < row; colIndex++) {
+      const location = `${rowIndex}-${colIndex}`;
+      unvisited.add(location);
+
+      if (rowIndex === 0 || rowIndex === maxRows - 1 || colIndex === 0 || colIndex === row - 1) {
+        coastTiles.push({ r: rowIndex, c: colIndex, key: `${rowIndex}-${colIndex}` });
+      }
+    }
+  });
+
+  const startTile = coastTiles[Math.floor(Math.random() * coastTiles.length)];
+  const cw = Math.random() > 0.5;
+
+  const isValid = (location) => unvisited.has(location);
+
+  const getDirection = (config, tile) => {
+    for (let dir = 0; dir < 6; dir++) {
+      const neighbour = getNeighbour(config, tile.r, tile.c, dir);
+
+      if (!isValid(neighbour.key)) {
+        continue
+      };
+
+      const nextNeighbour = getNeighbour(config, neighbour.r, neighbour.c, cw ? (dir + 5) % 6 : (dir + 1) % 6);
+
+      if (!isValid(nextNeighbour.key)) {
+        return dir;
+      }
+    }
+  }
+
+  const path = [];
+
+  let currentTile = startTile;
+  let currentDir = getDirection(config, currentTile);
+
+  while (unvisited.size > 0) {
+    path.push(currentTile);
+
+    unvisited.delete(currentTile.key);
+
+    if (unvisited.size === 0) break;
+
+    currentDir = cw ? (currentDir + 5) % 6 : (currentDir + 1) % 6;
+    let nextTile = getNeighbour(config, currentTile.r, currentTile.c, currentDir);
+
+    while (!isValid(nextTile.key)) {
+      currentDir = cw ? (currentDir + 1) % 6 : (currentDir + 5) % 6;
+      nextTile = getNeighbour(config, currentTile.r, currentTile.c, currentDir);
+    }
+
+    currentTile = nextTile;
+  }
+
+  const tokens = [...config.tokens];
+
+  path.forEach((location) => {
+    const hex = document.querySelector(`.hex[data-coords="${location.r}-${location.c}"]`);
+
+    if (hex && !hex.classList.contains('desert')) {
+      const number = tokens.shift();
+      const token = document.createElement('div');
+      token.classList.add('token', `num-${number}`);
+      token.textContent = number;
+      hex.appendChild(token);
+    }
+  });
+}
+
+const generateWater = (board, config, maxRows) => {
+  const topOffset = config.offset[0] - 0.5;
+  
+  for (let col = 0; col <= config.rows[0]; col++) {
+    generateTile(board, 'water', -1, col, topOffset);
+  }
+  
+  config.rows.forEach((row, rowIndex) => {
+    generateTile(board, 'water', rowIndex, -1, config.offset[rowIndex]);
+    generateTile(board, 'water', rowIndex, row, config.offset[rowIndex]);
+  });
+
+  const bottomOffset = config.offset[maxRows - 1] - 0.5;
+
+  for (let col = 0; col <= config.rows[maxRows - 1]; col++) {
+    generateTile(board, 'water', maxRows, col, bottomOffset);
+  }
+}
+
+const generateHarbours = (board, config, harbours) => {
+  config.harbourLocations.forEach((harbourLocation, index) => {
+    const hex = board.querySelector(`.hex[data-coords="${harbourLocation}"]`);
+
+    const harbourTiles = harbours === 'standard' ? [...config.harbourTypes] : shuffle([...config.harbourTypes]);
+
+    const harbourTile = document.createElement('div');
+    harbourTile.classList.add('harbour', harbourTiles[index]);
+    harbourTile.style.setProperty('transform', `rotate(${config.harbourRotations[index]})`);
+    hex.append(harbourTile);
+  });
+}
+
 const generateBoard = (players, harbours) => {
   const board = document.getElementById('board');
   board.innerHTML = '';
@@ -142,8 +193,14 @@ const generateBoard = (players, harbours) => {
   document.documentElement.style.setProperty('--max-rows', maxRows);
   document.documentElement.style.setProperty('--max-cols', maxCols);
 
+  generateWater(board, config, maxRows);
+
+  if (harbours !== 'off') {
+    generateHarbours(board, config, harbours);
+  }
+
   const tiles = shuffle([...config.pool]);
-  const tokens = shuffle([...config.tokens]);
+  const tokens = [...config.tokens];
 
   config.rows.forEach((count, rowIndex) => {
     const rowOffset = config.offset[rowIndex];
@@ -151,49 +208,18 @@ const generateBoard = (players, harbours) => {
     for (let colIndex = 0; colIndex < count; colIndex++) {
       const tile = tiles.pop();
 
-      const hex = document.createElement('div');
-      hex.classList.add('hex', tile);
-      hex.style.setProperty('--row', rowIndex);
-      hex.style.setProperty('--col', colIndex);
-      hex.style.setProperty('--row-offset', rowOffset);
-
-      board.appendChild(hex);
-
-      if (tile !== 'desert') {
-        const token = tokens.pop();
-
-        const number = document.createElement('div');
-        number.classList.add('token', `num-${token}`);
-        number.textContent = token;
-
-        hex.appendChild(number);
-      }
-
-      const harbourConfig = config.harbours.find((h) => h.location === `${rowIndex}-${colIndex}`)
-
-      if (harbours && harbourConfig) {
-        const harbour = document.createElement('div');
-
-        harbour.classList.add('harbour');
-        harbour.style.setProperty('--row', rowIndex);
-        harbour.style.setProperty('--col', colIndex);
-        harbour.style.setProperty('--row-offset', rowOffset);
-        harbour.style.setProperty('top', (hex.offsetTop + hex.clientHeight / 2) - hex.clientHeight / 4);
-        harbour.style.setProperty('left', (hex.offsetLeft + hex.clientWidth / 2) - hex.clientHeight / 4);
-        harbour.style.setProperty('transform', `rotate(${harbourConfig.rotation}) translateY(-${hex.clientHeight / 4 + hex.clientWidth / 2}px)`);
-        harbour.textContent = harbourConfig.type;
-
-        board.appendChild(harbour);
-      }
+      generateTile(board, tile, rowIndex, colIndex, rowOffset);
     }
   });
+
+  generateTokens(config);
 }
 
 const form = document.getElementById('settings');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  generateBoard(parseInt(form.querySelector('[name=players]').value), form.querySelector('[name=harbours]').checked);
+  generateBoard(parseInt(form.querySelector('[name=players]').value), form.querySelector('[name=harbours]').value);
 });
 
 document.addEventListener('keyup', (e) => {
